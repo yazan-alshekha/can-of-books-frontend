e6d3ce680d2ec8a3efd1c6e6b31d940d9f5d3b48
yazan alshekha <yazan.alshekha@outlook.com>
2021-08-24 21:50:24 +0300

    get data from api

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,8 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import axios from "axios";
 
 class MyFavoriteBooks extends React.Component {
+
+  async componentDidMount() {
+    if (this.props.auth0.isAuthenticated) {
+        let url = `http://localhost:3001/book?userName=${this.props.auth0.user.email}`;
+
+        console.log(this.props.auth0.user.email);
+        let userData = await axios.get(url);
+        console.log(userData.data);
+    }
+
+}
+
+
   render() {
     return(
       <Jumbotron>
@@ -16,4 +31,4 @@ class MyFavoriteBooks extends React.Component {
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0( MyFavoriteBooks);
